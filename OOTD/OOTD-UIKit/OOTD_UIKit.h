f80af592cd33410d01fc994980ce5ed9054674d9
김태현 <taehyk528@gmail.com>
2022-09-12 18:19:24 +0900

    [#7] UIKit Framework 생성

---
+++ OOTD/OOTD-UIKit/OOTD_UIKit.h
@@ -0,0 +1,18 @@
+//
+//  OOTD_UIKit.h
+//  OOTD-UIKit
+//
+//  Created by taekki on 2022/09/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OOTD_UIKit.
+FOUNDATION_EXPORT double OOTD_UIKitVersionNumber;
+
+//! Project version string for OOTD_UIKit.
+FOUNDATION_EXPORT const unsigned char OOTD_UIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OOTD_UIKit/PublicHeader.h>
+
+
