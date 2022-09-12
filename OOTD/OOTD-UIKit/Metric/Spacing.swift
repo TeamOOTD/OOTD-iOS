9a99f2eb5ced2ce71e9e8d1eee4787dc04334088
김태현 <taehyk528@gmail.com>
2022-09-12 20:25:02 +0900

    [#8] Spacing 추가

---
+++ OOTD/OOTD-UIKit/Metric/Spacing.swift
@@ -0,0 +1,18 @@
+//
+//  Spacing.swift
+//  OOTD-UIKit
+//
+//  Created by taekki on 2022/09/12.
+//
+
+import UIKit
+
+public enum Spacing {
+    public static let s4: CGFloat = 4
+    public static let s8: CGFloat = 8
+    public static let s16: CGFloat = 16
+    public static let s20: CGFloat = 20
+    public static let s24: CGFloat = 24
+    public static let s32: CGFloat = 32
+    public static let s64: CGFloat = 64
+}
