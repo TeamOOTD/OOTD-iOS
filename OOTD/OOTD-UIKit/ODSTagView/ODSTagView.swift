e0a361888660c6c11a2f3b4d576fe874688e3fc1
김태현 <taehyk528@gmail.com>
2022-09-13 00:30:32 +0900

    [#11] ODSTagView 구현

---
+++ OOTD/OOTD-UIKit/ODSTagView/ODSTagView.swift
@@ -0,0 +1,40 @@
+//
+//  ODSTagView.swift
+//  OOTD-UIKit
+//
+//  Created by taekki on 2022/09/13.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+import OOTD_Core
+
+public final class ODSTagView: BaseView {
+    
+    private let contentLabel = UILabel()
+    
+    public var content: String? {
+        get { contentLabel.text }
+        set { contentLabel.text = newValue }
+    }
+    
+    public override func configureAttributes() {
+        makeRounded(radius: 2)
+        
+        contentLabel.do {
+            $0.textColor = .white
+            $0.font = .ootdFont(.bold, size: 8)
+        }
+    }
+    
+    public override func configureLayout() {
+        addSubview(contentLabel)
+        
+        contentLabel.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(Spacing.s4)
+        }
+    }
+}
