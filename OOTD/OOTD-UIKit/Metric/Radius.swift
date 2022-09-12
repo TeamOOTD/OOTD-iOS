299e3b8f22e332be091d53bfc0e613d711a90f00
김태현 <taehyk528@gmail.com>
2022-09-12 20:26:39 +0900

    [#8] Radius 추가

---
+++ OOTD/OOTD-UIKit/Metric/Radius.swift
@@ -0,0 +1,17 @@
+//
+//  Radius.swift
+//  OOTD-UIKit
+//
+//  Created by taekki on 2022/09/12.
+//
+
+import UIKit
+
+public enum Radii {
+    public static let r2: CGFloat = 2
+    public static let r4: CGFloat = 4
+    public static let r5: CGFloat = 5
+    public static let r10: CGFloat = 10
+    public static let r12: CGFloat = 12
+    public static let r20: CGFloat = 20
+}
