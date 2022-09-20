a5328ecbb4325d8b569ea416e0886293c1525d59
김태현 <taehyk528@gmail.com>
2022-09-20 16:12:15 +0900

    [#30] Observable Helper Class 구현

---
+++ OOTD/OOTD/Common/Observable.swift
@@ -0,0 +1,28 @@
+//
+//  Observable.swift
+//  OOTD
+//
+//  Created by taekki on 2022/09/20.
+//
+
+import Foundation
+
+final class Observable<T> {
+    
+    private var listener: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ completion: @escaping((T) -> Void)) {
+        completion(value)
+        listener = completion
+    }
+}
