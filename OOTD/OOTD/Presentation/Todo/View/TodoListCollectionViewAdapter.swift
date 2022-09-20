d5e6811ebb28cdfd952ed78d207e071e1ed77aac
김태현 <taehyk528@gmail.com>
2022-09-20 14:37:55 +0900

    [#30] TodoListCollectionViewAdapter 파일 생성

---
+++ OOTD/OOTD/Presentation/Todo/View/TodoListCollectionViewAdapter.swift
@@ -0,0 +1,13 @@
+//
+//  TodoListCollectionViewAdapter.swift
+//  OOTD
+//
+//  Created by taekki on 2022/09/20.
+//
+
+import UIKit
+
+final class TodoListCollectionViewAdapter: NSObject {
+    
+}
+
