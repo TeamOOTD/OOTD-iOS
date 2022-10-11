c2f9f26229bb7fdc4ee8d9266b66157f0f88bead
김태현 <taehyk528@gmail.com>
2022-10-11 17:07:20 +0900

    [#88] 포그라운드 알림 수신

--- OOTD/OOTD/Application/AppDelegate.swift
+++ OOTD/OOTD/Application/AppDelegate.swift
@@ -68,6 +68,10 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
                      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
     }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.badge, .banner, .list])
+    }
 }
 
 extension AppDelegate: MessagingDelegate {
