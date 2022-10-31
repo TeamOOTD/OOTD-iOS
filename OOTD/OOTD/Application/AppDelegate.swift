bedfc6f722a488248a1e57b6533858ce794624e1
김태현 <taehyk528@gmail.com>
2022-10-31 18:11:16 +0900

    ✂️  :: Firebase registration token print문 제거

--- OOTD/OOTD/Application/AppDelegate.swift
+++ OOTD/OOTD/Application/AppDelegate.swift
@@ -67,15 +67,12 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
 extension AppDelegate: MessagingDelegate {
     
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        print("Firebase registration token: \(String(describing: fcmToken))")
-        
         let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        
         NotificationCenter.default.post(
             name: Notification.Name("FCMToken"),
             object: nil,
             userInfo: dataDict
         )
-        // TODO: If necessary send token to application server.
-        // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
 }
